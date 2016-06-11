e3c5f3a53df4d03448e263fcd8eead5fdfd77b4b
viktor <viktor.bobryk@gmail.com>
2016-06-11 22:13:30 +0300

    create form and catalog of cities

---
+++ main_wether.js
@@ -0,0 +1,778 @@
+var moduleStat = (function (){
+
+	var catalog = [
+		{
+			/* city */
+			cityName: "Rivne",
+			days: 
+				{
+					day: "01",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "02",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "03",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "04",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "05",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "06",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "07",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "08",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "09",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "10",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "11",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "12",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "13",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "14",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "15",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "16",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "17",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "18",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "19",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "20",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "21",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, {
+					day: "22",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "23",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "24",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "25",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				},
+				{
+					day: "26",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "27",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 	
+				{
+					day: "28",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "29",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "30",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				},
+				
+		},
+		{
+		/* city */
+			cityName: "Lviv",
+			days: 
+				{
+					day: "01",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "02",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "03",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "04",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "05",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "06",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "07",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "08",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "09",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "10",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "11",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "12",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "13",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "14",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "15",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "16",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "17",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "18",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "19",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "20",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "21",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, {
+					day: "22",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "23",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "24",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "25",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				},
+				{
+					day: "26",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "27",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 	
+				{
+					day: "28",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "29",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "30",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+		},
+		{
+		/* city */
+			cityName: "Kyiv",
+			days: 
+				{
+					day: "01",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "02",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "03",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "04",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "05",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "06",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "07",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "08",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "09",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "10",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "11",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "12",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "13",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "14",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "15",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "16",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "17",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "18",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "19",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "20",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "21",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, {
+					day: "22",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "23",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "24",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "25",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				},
+				{
+					day: "26",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "27",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 	
+				{
+					day: "28",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "29",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "30",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+		},
+		{
+		/* city */
+			cityName: "London",
+			days: 
+				{
+					day: "01",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "02",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "03",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "04",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "05",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "06",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "07",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "08",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "09",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "10",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "11",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "12",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "13",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "14",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "15",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "16",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "17",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "18",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "19",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "20",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "21",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, {
+					day: "22",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "23",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "24",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "25",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				},
+				{
+					day: "26",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "27",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 	
+				{
+					day: "28",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "29",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+				{
+					day: "30",
+					temperature: 15,
+					hymidity: 70,
+					wind: 
+				}, 
+		},
+	];
+				 
+					
+					studentName: "Borya",
+					marks: [
+						{
+							mark: 0,
+							subject: "phisics",
+							date: "1 / Sept"
+						}, 
+
+						{
+							mark: 0,
+							subject: "css",
+							date: "10 / Sept"
+						},
+
+						{
+							mark: 0,
+							subject: "maths",
+							date: "1 / Sept"
+						}, 
+
+						{
+							mark: 7,
+							subject: "html",
+							date: "5 / Sept"
+						},
+
+						{
+							mark: 8,
+							subject: "js",
+							date: "10 / Sept"
+						}
+					]
+				
+
+})();
